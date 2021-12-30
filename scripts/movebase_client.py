05a230ad3ae23ce3c7eed21796691edaef8aae12
Ali <65722399+aliy98@users.noreply.github.com>
2021-12-30 17:43:27 +0100

    Update movebase_client.py

--- scripts/movebase_client.py
+++ scripts/movebase_client.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
+from time import sleep
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from std_srvs.srv import *
+import os
+import time
 
 """
     movebase client Node:
         gets desired position from user and sends it to movebase node using 
         actionlib and if the goal is not reached before timeout cancels it
 """
-active_ = False
-
-# enables movebase client node if user has chosen it in master node
-def movebase_client_switch(req):
-    global active_
-    active_ = req.data
-    res = SetBoolResponse()
-    res.success = True
-    res.message = 'Done!'
-    return res
 
 def movebase_clinet():
     # Create an action client called "move_base" with action definition file "MoveBaseAction"
@@ -27,40 +19,51 @@ def movebase_clinet():
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
+    os.system('cls||clear')
+    print("** MOVEBASE CLIENT NODE **\n")
     # Gets goal position from user
-    print("Set new goal point:")
-    goal.target_pose.pose.position.x = float(input("Enter goal x position: "))
-    goal.target_pose.pose.position.y = float(input("Enter goal y position: "))
-
-    # No rotation of the mobile base frame w.r.t. map frame
-    goal.target_pose.pose.orientation.w = 1.0
-    # Waits until the action server has started up and started listening for goals.
-    client.wait_for_server()
-    # Sends the goal to the action server.
-    client.send_goal(goal)
-    finished_before_timeout = client.wait_for_result(timeout=rospy.Duration(30))
-    # detects if the target is reached before timeout
-    if finished_before_timeout:
-        print("Target reached!")
-        return client.get_result()
+    print("\nSet new goal point:")
+    x = input("Enter goal x position or q to change robot behaviour: ")
+    if x == 'q':
+        rospy.set_param('robot_state', '0')
+        print("\nchoose robot behaviour in master node")
+        time.sleep(5)
+        os.system('cls||clear')
+        print("** MOVEBASE CLIENT NODE **\n")
+        print("waiting for master node response...\n")
     else:
-        print("Action did not finish before time out!")
-        client.cancel_all_goals()
+        goal.target_pose.pose.position.x = float(x)
+        goal.target_pose.pose.position.y = float(input("Enter goal y position: "))
+        # No rotation of the mobile base frame w.r.t. map frame
+        goal.target_pose.pose.orientation.w = 1.0
+        # Waits until the action server has started up and started listening for goals.
+        client.wait_for_server()
+        # Sends the goal to the action server.
+        client.send_goal(goal)
+        finished_before_timeout = client.wait_for_result(timeout=rospy.Duration(30))
+        # detects if the target is reached before timeout
+        if finished_before_timeout:
+            print("Target reached!")
+            return client.get_result()
+        else:
+            print("Action did not finish before time out!")
+            client.cancel_all_goals()
+
 
 def main():
-    global active_
     rospy.init_node('movebase_client')
-    srv = rospy.Service('movebase_client_switch', SetBool, movebase_client_switch)
+    rospy.set_param('robot_state', '0')
+    os.system('cls||clear')
+    print("** MOVEBASE CLIENT NODE **\n")
+    print("waiting for master node response...\n")
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
-        if not active_:
+        if rospy.get_param('robot_state')=='1':
+            movebase_clinet()
+        else:
             rate.sleep()
             continue
-        else:
-            movebase_clinet()
         rate.sleep()
 
 if __name__ == '__main__':
     main()
-
-
