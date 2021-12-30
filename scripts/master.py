c417139a8bc74bbf8451cf2c92abef836ec6cf85
Ali <65722399+aliy98@users.noreply.github.com>
2021-12-30 17:47:52 +0100

    Update master.py

--- scripts/master.py
+++ scripts/master.py
@@ -1,65 +1,51 @@
 #!/usr/bin/env python
 import rospy
 from std_srvs.srv import *
-import time
+import os
 
 """
     master node:
         gets user request to choose robot behaviour
         using the following services
 """
-srv_client_movebase_client_ = None
-srv_client_teleop_keyboard_ = None
-srv_client_assisted_teleop_ = None
-state_desc_ = ['movebase client', 'teleop keyboard', 'assisted teleop']
-state_ = 0
-# 0 - movebase client
-# 1 - teleop keyboard
-# 2 - assisted teleop
 
-# selects robot's behaviour and sends the corresponding response to other three nodes
-def select_state(state):
-    global state_, state_desc_
-    global srv_client_movebase_client_, srv_client_teleop_keyboard_, srv_client_assisted_teleop_
-    state_ = state
-    print("state selected: %s"  % state_desc_[state])
-    if state_ == 0:
-        resp = srv_client_movebase_client_(True)
-        resp = srv_client_teleop_keyboard_(False)
-        resp = srv_client_assisted_teleop_(False)
-    if state_ == 1:
-        resp = srv_client_movebase_client_(False)
-        resp = srv_client_teleop_keyboard_(True)
-        resp = srv_client_assisted_teleop_(False)
-    if state_ == 2:
-        resp = srv_client_movebase_client_(False)
-        resp = srv_client_teleop_keyboard_(False)
-        resp = srv_client_assisted_teleop_(True)
+# 1 - movebase client
+# 2 - teleop keyboard
+# 3 - assisted teleop
 
-def main():
-    time.sleep(2)
-    global srv_client_movebase_client_, srv_client_teleop_keyboard_, srv_client_assisted_teleop_
-    rospy.init_node('master')
-    srv_client_movebase_client_ = rospy.ServiceProxy(
-        '/movebase_client_switch', SetBool)
-    srv_client_teleop_keyboard_ = rospy.ServiceProxy(
-        '/teleop_keyboard_switch', SetBool)
-    srv_client_assisted_teleop_ = rospy.ServiceProxy(
-        '/assisted_teleop_switch', SetBool)
-
+# changes robot's behaviour and sends the corresponding response to other three nodes
+def change_state():
+    os.system('cls||clear')
+    print("** MASTER NODE **\n")
     # gets robot behaviour from user
     x = input('''Choose robot behaviour:
     1. reach point(x,y) autonomously
     2. drive with keyboard
-    3. drive robot with collision avoidance
-    ''')
+    3. drive robot with collision avoidance\n
+    input: ''')
 
     if x == '1':
-        select_state(0)
+        rospy.set_param('robot_state', '1')
+        print("\nstate changed: movebase client")
     elif x == '2':
-        select_state(1)
+        rospy.set_param('robot_state', '2')
+        print("\nstate changed: teleop keyboard")
     elif x == '3':
-        select_state(2)
+        rospy.set_param('robot_state', '3')
+        print("\nstate changed: assisted teleop")
+
+
+def main():
+    rospy.init_node('master')
+    rospy.set_param('robot_state', '0')
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        if rospy.get_param('robot_state')=='0':
+            change_state()
+        else:
+            rate.sleep()
+            continue
+        rate.sleep()
 
 # If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
